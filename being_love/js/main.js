e76b23d2a5082646b84e045792bb9b18cad9eb29
Eva Lin <38998296+e30422456@users.noreply.github.com>
2021-08-30 18:01:00 +0800

    Add files via upload

---
+++ being_love/js/main.js
@@ -0,0 +1,42 @@
+//loading
+$(window).load(function(){
+        $('#loading').delay(500).fadeOut(3000);
+        });
+
+//scroll left and top
+/*$(function() {
+ $("body").mousewheel(function(event, delta) {
+  if($("body").width() <= 768){
+      this.scrollTop -= (delta * 100); 
+  }else if($("body").width() > 768){
+      this.scrollLeft -= (delta * 40);
+  }
+//  e.preventDefault();
+ });
+})*/
+
+//overlay
+$('.toggle').click(function() {
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
+	$('#header_left').toggleClass('change');
+});
+
+//LocomotiveScroll
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  repeat: false,
+  direction:"horizontal",
+    tablet: {
+      smooth: true,
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      breakpoint: 1024
+    },
+    smartphone: {
+      smooth: true,
+      direction: 'vertical',
+      gestureDirection: 'vertical'
+    }
+});
